d7a4d59e3c0b9f78b6e15864d58f19e33d8e1a65
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-08 15:38:35 +0100

    add card values

--- app/Card.js
+++ app/Card.js
@@ -1,3 +1,7 @@
+export const Weights = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+export const Types = ["spades", "hearts", "diamonds", "clubs"];
+
 export class Card {
     mapTextToSIgn = {
         hearts: "&hearts;",
@@ -15,7 +19,7 @@ export class Card {
         const card = document.createElement('div');
         card.setAttribute('class', `card ${this.type}`);
         card.innerHTML = `${this.weight} ${this.mapTextToSIgn[this.type]}`;
-        
+
         return card
     }
 
