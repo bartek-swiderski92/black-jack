025ca9b542fc3d4f8682ff85faf34c172fa1bd4a
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-08 15:52:43 +0100

    add initial game logic

--- app/Game.js
+++ app/Game.js
@@ -1,3 +1,30 @@
 import { Deck } from "./Deck.js";
+import { Player } from "./Player.js";
 
-const deck = new Deck()
+class Game {
+    constructor({ playerCards, dealerCards, player }) {
+        this.player = player;
+        this.playerCards = playerCards;
+        this.dealerCards = dealerCards;
+    }
+
+    run() {
+        const deck = new Deck();
+        deck.shuffle();
+    }
+
+    dealCards() {
+        for (let n = 0; n < 2; n++) {
+
+            
+        }
+    }
+
+}
+
+const player = new Player('Player');
+const game = new Game({
+    player,
+    playerCards: document.getElementById('playerCards'),
+    dealerCards: document.getElementById('dealerCards')
+});
