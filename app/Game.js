8c543dfe54f52cdca1c4f953b96816779cb7fa54
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-08 16:49:09 +0100

    add hitTable()

--- app/Game.js
+++ app/Game.js
@@ -1,39 +1,61 @@
 import { Player } from "./Player.js";
+import { Table } from "./Table.js";
 import { Deck } from "./Deck.js";
 
+const player = new Player('Player');
+const dealer = new Player('Dealer');
+const deck = new Deck();
+
 class Game {
-    constructor({ playerCards, dealerCards, player }) {
+    constructor({ player, dealer, table, deck, hitButton, stayButton }) {
         this.player = player;
-        this.dealer = new Player('Dealer')
-        this.playerCards = playerCards;
-        this.dealerCards = dealerCards;
-        this.deck = new Deck();
+        this.dealer = dealer
+        this.table = table;
+        this.deck = deck;
         this.deck.shuffle();
+        this.hitButton = hitButton;
+        this.stayButton = stayButton;
     }
 
     run() {
-        this.dealCards()
+        this.hitButton.addEventListener('click', () => this.hitCard());
+        this.dealCards();
+    }
+
+    hitCard() {
+        const card = this.deck.pickOne();
+        this.player.hand.addCard(card);
+        this.table.showPlayerCard(card);
     }
 
     dealCards() {
         for (let n = 0; n < 2; n++) {
             let card1 = this.deck.pickOne();
             this.player.hand.addCard(card1);
-            this.playerCards.appendChild(card1.render());
+            this.table.showPlayerCard(card1);
 
             let card2 = this.deck.pickOne();
             this.dealer.hand.addCard(card2);
-            this.dealerCards.appendChild(card2.render());
+            this.table.showDealerCard(card2);
         }
     }
 
 }
 
-const player = new Player('Player');
+
+const table = new Table(
+    document.getElementById('playerCards'),
+    document.getElementById('dealerCards')
+);
+
 const game = new Game({
     player,
-    playerCards: document.getElementById('playerCards'),
-    dealerCards: document.getElementById('dealerCards')
+    dealer,
+    table,
+    deck,
+    hitButton: document.getElementById('hit'),
+    stayButton: document.getElementById('stay'),
 });
+
 
 game.run();
