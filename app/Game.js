201ce2a0137d2a3c690294f952a0e5bb833f1329
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-09 10:06:20 +0100

    add endGame condition and messages

--- app/Game.js
+++ app/Game.js
@@ -1,13 +1,14 @@
 import { Player } from "./Player.js";
 import { Table } from "./Table.js";
 import { Deck } from "./Deck.js";
+import { Message } from "./Message.js";
 
 const player = new Player('Player');
 const dealer = new Player('Dealer');
 const deck = new Deck();
 
 class Game {
-    constructor({ player, dealer, table, deck, hitButton, stayButton, dealerPoints, playerPoints }) {
+    constructor({ player, dealer, table, deck, hitButton, stayButton, dealerPoints, playerPoints, messageBox }) {
         this.player = player;
         this.dealer = dealer
         this.table = table;
@@ -17,6 +18,7 @@ class Game {
         this.stayButton = stayButton;
         this.dealerPoints = dealerPoints;
         this.playerPoints = playerPoints;
+        this.messageBox = messageBox;
     }
 
     run() {
@@ -31,6 +33,9 @@ class Game {
         this.table.showPlayerCard(card);
         this.playerPoints.innerHTML = this.player.calculatePoints();
 
+        if (this.player.points > 21) {
+            this.endGame();
+        };
     }
 
     dealCards() {
@@ -46,6 +51,8 @@ class Game {
 
         this.playerPoints.innerHTML = this.player.calculatePoints();
         this.dealerPoints.innerHTML = this.dealer.calculatePoints();
+
+
     }
 
     dealerPlays() {
@@ -55,15 +62,55 @@ class Game {
             this.table.showDealerCard(card);
             this.dealerPoints.innerHTML = this.dealer.calculatePoints()
         }
+        this.endGame();
     }
 
-}
+    endGame() {
+        this.hitButton.removeEventListener('click', (event) => this.hitCard());
+        this.stayButton.removeEventListener('click', (event) => this.dealerPlays());
 
+        this.hitButton.style.display = 'none';
+        this.stayButton.style.display = 'none';
+
+
+        if (this.player.points < 21 && this.player.points == this.dealer.points) {
+            this.messageBox.setText('Tie!').show();
+
+            return
+        }
+
+        if (this.player.points > 21) {
+            this.messageBox.setText('Dealer wins!').show();
+
+            return
+        }
+
+        if (this.dealer.points > 21) {
+            this.messageBox.setText('Player wins!').show();
+
+            return
+        }
+
+        if (this.player.points > 21) {
+            this.messageBox.setText('Player wins!').show();
+
+            return
+        }
+
+        if (this.player.points < this.dealer.points) {
+            this.messageBox.setText('Dealer wins!').show();
+
+            return
+        }
+
+    }
+}
 
 const table = new Table(
     document.getElementById('playerCards'),
     document.getElementById('dealerCards')
 );
+const messageBox = new Message(document.getElementById('message'));
 
 const game = new Game({
     player,
@@ -74,6 +121,7 @@ const game = new Game({
     stayButton: document.getElementById('stay'),
     dealerPoints: document.getElementById('dealerPoints'),
     playerPoints: document.getElementById('playerPoints'),
+    messageBox
 });
 
 
